d13323fda841b5ea1ca72ed444aa767e2ed0f7ab
sudhir <pawarsudhir84@gmail.com>
2020-11-02 17:16:01 +0530

    Updated validator.java

--- src/main/Validator.java
+++ src/main/Validator.java
@@ -17,13 +17,18 @@ public class Validator {
 		return Pattern.compile("A|B|C").matcher(division).find();
 	}
 	public static boolean isValidDepartment(String department){
-		return Pattern.compile("COMP|IT|E&TC|MECH|CIVIL").matcher(department).find();
+		return Pattern.compile("COMP|IT|E&TC|MECH|CIVIL|comp|it|e&tc|mech|civil").matcher(department).find();
 	}
 	public static boolean isValidContactNo(String contactNo){
 		return Pattern.compile("^[0-9]{10}$").matcher(contactNo).find();
 	}
 	public static boolean isValidDate(String date){
 		return Pattern.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}$").matcher(date).find();
+	}
+	public static String isValidUser(String userId, String department, String year, String division, String contactNo){
+		String invalidFields = "";
+		invalidFields = invalidFields.concat(isValidUserId(userId) ?  "" : "UserId ").concat(isValidDepartment(department) ? "" : "department ").concat(isValidYear(year)?"":"year ").concat(isValidDivision(division) ? "" : "division ").concat(isValidContactNo(contactNo)? "" :"contactNo");
+		return invalidFields;
 	}
 	
 }
