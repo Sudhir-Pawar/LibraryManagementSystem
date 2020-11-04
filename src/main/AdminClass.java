1fcf1ebfe1a3bfa66de3efab208b26a8e69268fe
sudhir <pawarsudhir84@gmail.com>
2020-11-04 22:45:49 +0530

    Updated AdminClass.java

--- src/main/AdminClass.java
+++ src/main/AdminClass.java
@@ -26,7 +26,6 @@ public class AdminClass {
 		String division = bufferedReader.readLine();
 		System.out.println("Enter Contact number: ");
 		String contactNo = bufferedReader.readLine();
-		System.out.println(userId+" "+username);
 		String inValidFieldsValidator = Validator.isValidUser(userId, department, year, division, contactNo);
 		if(inValidFieldsValidator.isEmpty()){
 			return new DaoClass().addUser(new User(userId,username,department,year,division,contactNo));			
